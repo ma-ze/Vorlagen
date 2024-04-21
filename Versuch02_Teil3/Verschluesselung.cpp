fd95a4ed6fc0f0542344317495ea61d6cc41d70f
Martin <34374148+ma-ze@users.noreply.github.com>
2024-04-21 21:32:59 +0200

    add some comments

--- Versuch02_Teil3/Verschluesselung.cpp
+++ Versuch02_Teil3/Verschluesselung.cpp
@@ -14,8 +14,8 @@ using namespace std;
 
 string verschluesseln(char schluessel[2][26], string wort)
 {
-	for(int i=0; i < wort.length(); i++){
-		wort[i] = schluessel[1][wort[i]-65];
+	for(int i=0; i < wort.length(); i++){ 
+		wort[i] = schluessel[1][wort[i]-65]; //find the corresponding letter in the cypher, taking advantage of the order of the alphabet in the ASCII Format.
 	}
 
 	return wort;
@@ -25,8 +25,8 @@ string entschluesseln(char schluessel[2][26], string wort)
 {
 	for(int i=0; i < wort.length(); i++){
 		for(int k=0; k<26; k++){
-			if(wort[i] == schluessel[1][k]){
-				wort[i] = schluessel[0][k];
+			if(wort[i] == schluessel[1][k]){ //loop through the cypher to find the current letter
+				wort[i] = schluessel[0][k]; //replace letter with deciphered letter
 				break;
 			}
 		}
@@ -40,7 +40,6 @@ int main()
 	 char supersecretLUT[2][26] = {	{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'},
 	 								{'G', 'O', 'K', 'F', 'T', 'J', 'X', 'V', 'Y', 'A', 'S', 'D', 'M', 'N', 'H', 'I', 'Q', 'P', 'L', 'U', 'B', 'W', 'Z', 'C', 'R', 'E'}};
 
-	
 	string testwort;
 	cout << "Enter one word in ALL CAPS:\n";
 	getline(cin, testwort);
