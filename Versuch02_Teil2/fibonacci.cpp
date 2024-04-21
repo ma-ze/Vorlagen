b6ee4ab29f0a19c1a010d4fe0818df9d560cb90e
Martin <34374148+ma-ze@users.noreply.github.com>
2024-04-21 20:11:41 +0200

    Versuch02Teil2

--- Versuch02_Teil2/fibonacci.cpp
+++ Versuch02_Teil2/fibonacci.cpp
@@ -7,10 +7,17 @@
 //////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
+// Aufgabe 2.2.1
+int fib_rec(int n){ 
+    if(n==0) return 0;
+    if(n==1) return 1;
+    return fib_rec(n-1) + fib_rec(n-2);
+}
+
 
 int fibonacci(int n)
 {
-    int previousFib = 1;
+    int previousFib = 0;
     int currentFib = 1;
     if(n == 0)
     {
@@ -20,17 +27,22 @@ int fibonacci(int n)
     {
         return 1;
     }
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i <= n-2; i++)
     {
         int newFib = previousFib + currentFib ;
-        previousFib = newFib ;
-        currentFib = previousFib ;
+        previousFib = currentFib ;
+        currentFib = newFib ;
     }
     return currentFib ;
 }
 
 int main ()
 {
+    std::cout << " Fibonacci - Zahlen von 0 bis 25:" << std::endl ;
+    for (int i = 0; i <= 25 ; i ++)
+    {
+        std::cout << "f(" << i << ") = " << fib_rec(i) << std::endl ;
+    }
     std::cout << " Fibonacci - Zahlen von 0 bis 12:" << std::endl ;
     for (int i = 1; i <= 12 ; i ++)
     {
