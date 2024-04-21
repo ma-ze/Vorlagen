f9019e5a9eae09d77ad7e9f546933a49350a2977
Martin <34374148+ma-ze@users.noreply.github.com>
2024-04-21 19:49:02 +0200

    Versuch 01teil2

--- Versuch01_Teil2/Strukturen.cpp
+++ Versuch01_Teil2/Strukturen.cpp
@@ -8,9 +8,52 @@
 
 #include <iostream>
 #include <string>
+using namespace std;
+
+struct Person{
+    string sNachname, sVorname;
+    int iGeburtsjahr, iAlter;
+};
+ostream& operator <<(ostream& os, const Person& person) {
+    return os   << "First name:    " << person.sVorname << endl
+                << "Last name:     " << person.sNachname << endl
+                << "Year of birth: " << person.iGeburtsjahr << endl
+                << "Age:           " << person.iAlter << endl;
+}
 
 int main()
 {
+    Person nBenutzer;
+    cout << "last name?\n";
+    getline(cin, nBenutzer.sNachname);
+    cout << "first name?\n";
+    getline(cin, nBenutzer.sVorname);
+    cout << "year of birth?\n";
+    string input;
+    getline(cin, input);
+    nBenutzer.iGeburtsjahr = stoi(input);
+    cout << "how old?\n";
+    getline(cin, input);
+    nBenutzer.iAlter = stoi(input);
+    cout << "\n";
+    cout << "You have entered the following information: ";
+    cout << nBenutzer.sVorname + " " + nBenutzer.sNachname + ", ";
+    cout << "born " << nBenutzer.iGeburtsjahr << ", ";
+    cout << "age: " << nBenutzer.iAlter << "\n";
+    
+    cout << "\n";
+    Person nKopieEinzeln, nKopieGesamt;
+    nKopieEinzeln.sNachname = nBenutzer.sNachname;
+    nKopieEinzeln.sVorname = nBenutzer.sVorname;
+    nKopieEinzeln.iGeburtsjahr = nBenutzer.iGeburtsjahr;
+    nKopieEinzeln.iAlter = nBenutzer.iAlter;
+    
+    cout << "\n";
+    cout << nKopieEinzeln;
+    
+    nKopieGesamt = nBenutzer;
+    cout << "\n";
+    cout << nKopieGesamt;
     return 0;
     
 }
