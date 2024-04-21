72993f9ae8270323ebc27a49e865daa93144d828
Martin <34374148+ma-ze@users.noreply.github.com>
2024-04-21 21:18:23 +0200

    Versuch2Teil3

--- Versuch02_Teil3/Verschluesselung.cpp
+++ Versuch02_Teil3/Verschluesselung.cpp
@@ -7,28 +7,51 @@
 // Inhalt: Hauptprogramm
 //////////////////////////////////////////////////////////////////////////////
 
-using namespace std;
 
 #include <iostream>
 #include <string>
+using namespace std;
 
 string verschluesseln(char schluessel[2][26], string wort)
 {
-	// Hier folgt Ihr Code
+	for(int i=0; i < wort.length(); i++){
+		wort[i] = schluessel[1][wort[i]-65];
+	}
 
 	return wort;
 }
 
 string entschluesseln(char schluessel[2][26], string wort)
 {
-	// Hier folgt Ihr Code
+	for(int i=0; i < wort.length(); i++){
+		for(int k=0; k<26; k++){
+			if(wort[i] == schluessel[1][k]){
+				wort[i] = schluessel[0][k];
+				break;
+			}
+		}
+	}
 
 	return wort;
 }
 
 int main()
 {
-	// Hier folgt Ihr Code
+	 char supersecretLUT[2][26] = {	{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'},
+	 								{'G', 'O', 'K', 'F', 'T', 'J', 'X', 'V', 'Y', 'A', 'S', 'D', 'M', 'N', 'H', 'I', 'Q', 'P', 'L', 'U', 'B', 'W', 'Z', 'C', 'R', 'E'}};
 
+	
+	string testwort;
+	cout << "Enter one word in ALL CAPS:\n";
+	getline(cin, testwort);
+	cout << "\n";
+	cout << testwort << "  ----->  ";
+	cout << verschluesseln(supersecretLUT, testwort);
+	cout << "  ----->  " << entschluesseln(supersecretLUT, verschluesseln(supersecretLUT, testwort)) << "\n";
+	cout << "Enter one encoded word, for example \"VTDDHZHPDF\":\n";
+	getline(cin, testwort);
+	cout << "\n";
+	cout << testwort << "  ----->  ";
+	cout << entschluesseln(supersecretLUT, testwort) << "\n";
 	return 0;
 }
