87440d36c33623718696603b9bbad0743174a8b9
Martin <34374148+ma-ze@users.noreply.github.com>
2024-04-21 20:35:44 +0200

    a few additions to the fib thing

--- Versuch02_Teil2/fibonacci.cpp
+++ Versuch02_Teil2/fibonacci.cpp
@@ -14,7 +14,6 @@ int fib_rec(int n){
     return fib_rec(n-1) + fib_rec(n-2);
 }
 
-
 int fibonacci(int n)
 {
     int previousFib = 0;
@@ -36,6 +35,27 @@ int fibonacci(int n)
     return currentFib ;
 }
 
+unsigned long longfib(int n)
+{
+    unsigned long previousFib = 0;
+    unsigned long currentFib = 1;
+    if(n == 0)
+    {
+        return 0;
+    }
+    if(n == 1)
+    {
+        return 1;
+    }
+    for (int i = 0; i <= n-2; i++)
+    {
+        unsigned long newFib = previousFib + currentFib ;
+        previousFib = currentFib ;
+        currentFib = newFib ;
+    }
+    return currentFib ;
+}
+
 int main ()
 {
     std::cout << " Fibonacci - Zahlen von 0 bis 25:" << std::endl ;
@@ -48,5 +68,13 @@ int main ()
     {
         std::cout << "f(" << i << ") = " << fibonacci(i) << std::endl ;
     }
+    std::cout << "results for n = 47:" << std::endl ;
+    //std::cout << fib_rec(47) << std::endl ; //comment this in if you are patient
+    std::cout << fibonacci(47) << std::endl ;
+    std::cout << longfib(47) << std::endl ;
+    std::cout << "fib(46):" << std::endl ;
+    std::cout << fibonacci(46) << std::endl ;
+    std::cout << "fib(93):" << std::endl ;
+    std::cout << longfib(93) << std::endl ; //last number that can be calculated with unsigned long (64 bit)
     return 0;
 }
