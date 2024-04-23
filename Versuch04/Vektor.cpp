d6740d757cfd3dbdbb48ad4c82a5c5bdfe78d9d5
Martin <34374148+ma-ze@users.noreply.github.com>
2024-04-23 20:32:09 +0200

    A4 beginning

--- Versuch04/Vektor.cpp
+++ Versuch04/Vektor.cpp
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////
 // Praktikum Informatik 1 
 // 
-// Versuch 04: Einführung Klasse
+// Versuch 04: Einfï¿½hrung Klasse
 //
 // Datei:  Vektor.cpp
 // Inhalt: Sourcedatei der Klasse Vektor
@@ -21,10 +21,71 @@ Vektor::~Vektor()
 
 /**
  * @brief Function printing the components of the vector 
+ * @return void
  */
 void Vektor::ausgabe() const
 {
     std::cout << "X-Komponente: " << x << std::endl;
     std::cout << "Y-Komponente: " << y << std::endl;
     std::cout << "Z-Komponente: " << z << std::endl << std::endl;
+}
+
+/**
+ * @brief Function to calculate the length of the vector
+ * @return The length of the vector
+ */
+double Vektor::laenge() const
+{
+    return sqrt(x*x + y*y + z*z);
+}
+
+/**
+ * @brief Function to subtract another vector from this vector
+ * @param input The vector to be subtracted
+ * @return The resulting vector after subtraction
+ */
+Vektor Vektor::sub(const Vektor& input) const
+{
+    double resX = x - input.x;
+    double resY = y - input.y;
+    double resZ = z - input.z;
+
+    return Vektor(resX, resY, resZ);
+}
+/**
+ * @brief Function to calculate the scalar product of two vectors
+ * @param input The other vector for scalar product calculation
+ * @return The scalar product of the two vectors
+ */
+double Vektor::skalarProd(const Vektor& input) const
+{
+    return x * input.x + y * input.y + z * input.z;
+}
+
+/**
+ * @brief Function to calculate the angle between two vectors in degrees
+ * @param input The other vector to calculate the angle with
+ * @return The angle between the two vectors in degrees
+ */
+double Vektor::winkel(const Vektor& input) const
+{
+    // Calculate the angle in radians
+    double angleRad = acos(skalarProd(input) / laenge() * input.laenge());
+
+    // Convert the angle from radians to degrees
+    return angleRad * 180.0 / M_PI;
+}
+
+/**
+ * @brief Function to rotate the vector around the z-axis by a specified angle in radians
+ * @param rad The angle in radians by which to rotate the vector
+ */
+void Vektor::rotiereUmZ(const double rad)
+{
+    //apply the transformatin matrix
+    double newX = x * cos(rad) - y * sin(rad);
+    double newY = x * sin(rad) + y * cos(rad);
+
+    x = newX;
+    y = newY;
 }
--- Versuch04/main.cpp
+++ Versuch04/main.cpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////////
 // Praktikum Informatik 1 MMXXIV
-// Versuch 04: Einführung Klasse
+// Versuch 04: Einfï¿½hrung Klasse
 //
 // Datei:  main.cpp
 // Inhalt: Hauptprogramm
@@ -10,12 +10,34 @@
 
 int main()
 {
-    Vektor vector1(1, 0, 0);
-    Vektor vector2(0, 1, 0);
-
-    vector1.ausgabe();
-    vector2.ausgabe();
-
+    Vektor e1(1, 0, 0);
+    Vektor e2(0, 1, 0);
+    Vektor e3(0, 0, 1);
+    Vektor testvektor(1,1,1);
+    std::cout << "Einheitsvektoren:\n";
+    e1.ausgabe();
+    e2.ausgabe();
+    e3.ausgabe();
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "Winkel e1-e2: " << e1.winkel(e2) << "\n";
+    std::cout << "Winkel e2-e3: " << e2.winkel(e3) << "\n";
+    std::cout << "Winkel e3-e1: " << e1.winkel(e3) << "\n";
+    std::cout << "\n";
+    std::cout << "Testvektor:\n";
+    testvektor.ausgabe();
+    std::cout << "Laenge: " << testvektor.laenge() << "\n";
+    std::cout << "Winkel zu e1: " << testvektor.winkel(e1) << "\n";
+    std::cout << "Winkel zu e2: " << testvektor.winkel(e2) << "\n";
+    std::cout << "Winkel zu e3: " << testvektor.winkel(e3) << "\n";
+    std::cout << "Rotieren um pi (180grad):\n";
+    testvektor.rotiereUmZ(M_PI);
+    testvektor.ausgabe();
+    std::cout << "neue winkel:\n";
+    std::cout << "Winkel zu e1: " << testvektor.winkel(e1) << "\n";
+    std::cout << "Winkel zu e2: " << testvektor.winkel(e2) << "\n";
+    std::cout << "Winkel zu e3: " << testvektor.winkel(e3) << "\n";
+    std::cout << "\n\n\n\n\n\n\n"; 
     return 0;
 
 }
